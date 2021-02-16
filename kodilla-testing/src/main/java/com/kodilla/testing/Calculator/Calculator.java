f1a3168ed3092b754982ddfb160e5a2e7168ab94
takado <kornelgut@gmail.com>
2021-02-16 02:48:22 +0100

    6.2 first test

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/Calculator/Calculator.java
@@ -0,0 +1,10 @@
+package com.kodilla.testing.Calculator;
+
+public class Calculator {
+    static public int add(int a, int b) {
+        return a + b;
+    }
+    static public int subtract(int a, int b) {
+        return a - b;
+    }
+}
--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,29 @@
 package com.kodilla.testing;
+import com.kodilla.testing.Calculator.Calculator;
+import java.util.Random;
 
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        System.out.println("Test - pierwszy test jednostkowy:");
+        Random rand = new Random();
+        boolean testOk = true;
+        for(int i=0; i<1000000; i++) {
+            int a = rand.nextInt();
+            int b = rand.nextInt();
+            if(a+b != Calculator.add(a,b)) {
+                System.out.println("Fail: " + a + "+" + b + "!=" + Calculator.add(a,b));
+                testOk = false;
+                break;
+            }
+            if(a-b != Calculator.subtract(a,b)) {
+                System.out.println("Fail: " + a + "+" + b + "!=" + Calculator.subtract(a, b));
+                testOk = false;
+                break;
+            }
+        }
+        if (testOk) {
+            System.out.println("Test OK");
+        }
+
     }
 }
