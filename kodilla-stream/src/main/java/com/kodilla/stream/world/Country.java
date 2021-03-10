7d83276d44da02de9ab93d60e3e84f564227154e
takado <kornelgut@gmail.com>
2021-03-10 19:13:43 +0100

    Update Country.java

--- kodilla-stream/src/main/java/com/kodilla/stream/world/Country.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/Country.java
@@ -3,7 +3,7 @@ package com.kodilla.stream.world;
 import java.math.BigDecimal;
 
 public class Country {
-    BigDecimal people_nb;
+    private final BigDecimal people_nb;
 
     public Country(BigDecimal people_nb) {
         this.people_nb = people_nb;
