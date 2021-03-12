29b353a2df2835b3a13f981d51c67f9bab7ecdb3
takado <kornelgut@gmail.com>
2021-03-12 22:40:02 +0100

    8.2 try catch finally

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,25 @@
+package com.kodilla.exception.test;
+
+public class FirstChallenge {
+    public double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        FirstChallenge firstChallenge = new FirstChallenge();
+        double result = Double.NaN;
+        try {
+            result = firstChallenge.divide(3, 0);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Following exception occurred: " + e);
+        }
+        finally {
+            System.out.println(result);
+        }
+    }
+}
+
